dd16a7669064f06799aa77e5ef4fd1ed9ccb97c9
Olesya Morozova <ovmorozova12@yandex.ru>
2024-06-07 00:15:40 +0400

    Parameterized tests

--- src/test/java/ru/netology/hw5exercise1/hw5exercise1/services/RestServiceTest.java
+++ src/test/java/ru/netology/hw5exercise1/hw5exercise1/services/RestServiceTest.java
@@ -2,33 +2,41 @@ package ru.netology.hw5exercise1.hw5exercise1.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RestServiceTest {
-    @Test
-    void restThreshold20000() {
+    @ParameterizedTest
+//    @CsvSource({
+//            "10000, 3000, 20000, 3",
+//            "100000, 60000, 150000, 2"
+//    })
+    @CsvFileSource(files = "src/test/resources/Rest.csv")
+    void restThreshold20000(int income, int expenses, int threshold, int expected) {
         RestService service = new RestService();
 
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
+//        int income = 10_000;
+//        int expenses = 3_000;
+//        int threshold = 20_000;
+//        int expected = 3;
 
         int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void restThreshold150000() {
-        RestService service = new RestService();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    void restThreshold150000() {
+//        RestService service = new RestService();
+//
+////        int income = 100_000;
+////        int expenses = 60_000;
+////        int threshold = 150_000;
+////        int expected = 2;
+//
+//        int actual = service.calculate(income, expenses, threshold);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
